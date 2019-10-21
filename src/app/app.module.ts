883ee92443f9f8564d08dc2b4882979f3b8fd603
Lakshmana Kumar <lakshmanj@ilink-systems.com>
2019-10-21 14:00:06 +0530

    Example done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { SnippetsModule } from './snippets/snippets.module';
+
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: './main/main.module#MainModule'
+  }
+];
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SnippetsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
